82a5262bdb7d26651a651ae6217485d49a63478a
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-11-24 15:02:59 -0600

    displaying titles on homepage

--- js/movie-app.js
+++ js/movie-app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const apiURL = 'https://knowing-healthy-price.glitch.me/db';
+const apiURL = 'https://knowing-healthy-price.glitch.me/movies'
 
 // @returns {Promise<void>}
 
@@ -8,6 +8,6 @@ const getMovies = () => fetch(apiURL)
     .then(res => res.json())
     .catch(console.error);
 
-console.log(getMovies());
+// console.log(getMovies());
 
 // console.log("hello");
