9210c116f3a0c6c41c38a2d27ea7bc9e31819124
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2020-11-25 23:10:50 -0600

    second try did a little better

--- js/movie-app.js
+++ js/movie-app.js
@@ -10,30 +10,97 @@ const getMovies = () => fetch(apiURL)
 //Setting to create new movie
 // const test = {title: 'Red', body: 'Blue!'};
 
-const addMovie = (addNewMovie) => fetch(`${apiURL}`, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(addNewMovie())
-})
-    .then(res => res.json())
-    .then(data => {
-        console.log(`Success: created ${JSON.stringify(data)}`);
-        return data.id;
+// const addMovie = (addNewMovie) => fetch(`${apiURL}`, {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify()
+// })
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(`Success: created ${JSON.stringify(data)}`);
+//         return data.id;
+//     })
+//     .catch(console.error);
+
+// function addNewMovie(){
+//     let newMovieTitle = newMovieTitleInput.value;
+//     let newMovie = {
+//         title: newMovieTitle
+//     }
+//     movies.push(newMovie)
+// }
+//
+// var movieInput = document.querySelector(#movie-input)
+//
+function addNewMovie (){
+    var formData = {
+        'title'              : $('input[name=title]').val(),
+        'rating'             : $('input[name=rating]').val(),
+        // 'superheroAlias'    : $('input[name=superheroAlias]').val()
+    };
+
+    // process the form
+    $.ajax({
+        type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+        url         : apiURL, // the url where we want to POST
+        data        : formData, // our data object
+        dataType    : 'json', // what type of data do we expect back from the server
+        encode          : true
     })
-    .catch(console.error);
+        // using the done promise callback
+        .done(function(data) {
 
-function addNewMovie(){
-    let newMovieTitle = newMovieTitleInput.value;
-    let newMovie = {
-        title: newMovieTitle
-    }
-    movies.push(newMovie)
+            // log data to the console so we can see
+            console.log(data);
+
+            // here we will handle errors and validation messages
+        });
+
+    // stop the form from submitting the normal way and refreshing the page
+    event.preventDefault();
 }
+// $("form").on("submit", function(event){
+//     event.preventDefault();
+//
+//     var formValues= $(this).serialize();
+//
+//     $.post("process_form.php", formValues, function(data){
+//         // Display the returned data in browser
+//         $("#result").html(data);
+//     });
+// });
+$('#movie-form').on("submit", function(event) {
+    // get the form data
+    // there are many ways to get this data using jQuery (you can use the class or id also)
+    event.preventDefault();
+    var formData = {
+        'title'              : $('input[name=title]').value(),
+        'rating'             : $('input[name=rating]').value(),
+        // 'superheroAlias'    : $('input[name=superheroAlias]').val()
+    };
 
-var movieInput = document.querySelector(#movie-input)
+    // process the form
+    $.ajax({
+        type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+        url         : apiURL, // the url where we want to POST
+        data        : formData, // our data object
+        dataType    : 'json', // what type of data do we expect back from the server
+        encode          : true
+    })
+        // using the done promise callback
+        .done(function(data) {
 
+            // log data to the console so we can see
+            console.log(data);
 
+            // here we will handle errors and validation messages
+        });
 
+    // stop the form from submitting the normal way and refreshing the page
+    return false;
+});
+
+// });
 
